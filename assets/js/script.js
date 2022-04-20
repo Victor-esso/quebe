49739ba3ba57edfac48c5fb6ce50be7414f4e2ad
Victor Esso <victoresso97@gmail.com>
2022-04-20 10:22:50 +0100

    me

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,38 @@ $(document).ready(function(){
     })
 
 
+    //Create Siper
+    $('.row-swiper').each(function(){
+        //Parameters
+        var swiperName = $(this).attr('swiper-name');
+        var swiperLoop = isDefined($(this).attr('swiper-loop'),true) ? true: false ;
+        var slidesPerView = isDefined($(this).attr('slides-per-view')) ? (numberOnly($(this).attr('slides-per-view')) > 0 ? numberOnly($(this).attr('slides-per-view')) : 'auto' ) : 'auto';
+        var direction = isDefined($(this).attr('direction')) ? ($(this).attr('direction').trim() == 'vertical' ? 'vertical' : 'horizontal'): 'horizontal';
+        var noFreeMode = isDefined($(this).attr('no-free-mode'),true) ? false: true ;
+        var centered = isDefined($(this).attr('centered'),true) ? true: false ;
+        var autoPlay = isDefined($(this).attr('slides-auto-play')) ? (numberOnly($(this).attr('slides-auto-play')) > 0 ? numberOnly($(this).attr('slides-auto-play')) : false) : false;
+
+        var autoPlay = autoPlay ? {delay: autoPlay , disableOnInteraction: false, pauseOnMouseEnter: true,} : false;
+
+        var swiper = new Swiper($(this)[0], {
+        // Optional parameters
+        direction: direction,
+        slidesPerView: slidesPerView,
+        loop: swiperLoop,
+        freeMode: noFreeMode,
+        centeredSlides: centered,
+        autoplay: autoPlay,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.'+swiperName+'-next',
+            prevEl: '.'+swiperName+'-prev',
+        },
+        });
+
+
+    })
+
+
     
 
 
